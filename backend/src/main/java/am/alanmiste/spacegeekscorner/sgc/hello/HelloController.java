5208085e65d16faff611faec1d76ed0d5ec83d2c
Alan <baranalaan@gmail.com>
2022-08-16 12:28:04 +0200

    Connect frontend to backend

---
+++ backend/src/main/java/am/alanmiste/spacegeekscorner/sgc/hello/HelloController.java
@@ -0,0 +1,12 @@
+package am.alanmiste.spacegeekscorner.sgc.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping(path = "/hello")
+    String sayHello() {
+        return "Hello!";
+    }
+}
