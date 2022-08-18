e91b0ad6925eeb5b4f2dcbece342c96977b2bf6d
Alan <baranalaan@gmail.com>
2022-08-18 20:40:37 +0200

    Add Rocket.svg for Loading

--- frontend/src/components/CardsList.tsx
+++ frontend/src/components/CardsList.tsx
@@ -1,5 +1,6 @@
 import {NasaResponseType} from "../type/NasaResponseType";
 import Card from "./Card";
+import "./CardsList.css";
 
 type CardsListProps = {
     nasaApiDataList: NasaResponseType[],
@@ -7,7 +8,7 @@ type CardsListProps = {
 
 export default function CardsList(props: CardsListProps) {
     return <>
-        {props.nasaApiDataList.length === 0 ? "Loading... " : props.nasaApiDataList.map(card =>
+        {props.nasaApiDataList.length === 0 ? <div className={"rocket"}></div> : props.nasaApiDataList.map(card =>
             <Card nasaApiData={card}/>
         )}
     </>
