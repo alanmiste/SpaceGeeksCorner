b7c6164953de6873b56977bc393e3ada1cff8e4d
Alan <baranalaan@gmail.com>
2022-08-20 20:12:56 +0200

    Add key to Card, delete some comments and empty lines.

--- frontend/src/components/CardsList.tsx
+++ frontend/src/components/CardsList.tsx
@@ -9,7 +9,7 @@ type CardsListProps = {
 export default function CardsList(props: CardsListProps) {
     return <div className={"cardList"}>
         {props.nasaApiDataList.length === 0 ? <div className={"rocket"}></div> : props.nasaApiDataList.map(card =>
-            <Card nasaApiData={card}/>
+            <Card key={card.url} nasaApiData={card}/>
         )}
     </div>
 }
