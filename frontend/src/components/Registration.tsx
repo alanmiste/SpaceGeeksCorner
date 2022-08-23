956041fbd51dba7de5808ae1821514f97d27d7e8
Alan <baranalaan@gmail.com>
2022-08-23 17:04:11 +0200

    Update Registration.tsx and Registration.css, add Toastify, Update MyAccount.tsx

--- frontend/src/components/Registration.tsx
+++ frontend/src/components/Registration.tsx
@@ -1,102 +1,119 @@
 import "./Registration.css";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
+import {toast} from "react-toastify";
 
 export default function Registration() {
 
+    const [me, setMe] = useState<string>();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    const hideAndShow = () => {
-        const el = document.getElementById('loginContainer');
-
-        if (el != null) {
-            el.addEventListener('click', function handleClick() {
-                if (el.style.display === 'none') {
-                    // ✅ Shows element if hidden
-                    el.style.display = 'block';
-
-                    el.textContent = 'Hide element';
-                } else {
-                    // ✅ Hides element if shown
-                    el.style.display = 'none';
-
-                    el.textContent = 'Show element';
-                }
-            });
-        }
+    const fetchMe = () => {
+        axios.get("/api/users/me")
+            .then(response => response.data)
+            .then(setMe)
     }
+
+    useEffect(
+        () => {
+            fetchMe();
+            axios.interceptors.response.use(response => response, error => {
+                const status = error.response ? error.response.status : null;
+                if (status === 401 && !error.config.auth) {
+                    toast("Session timed out")
+                    fetchMe()
+                }
+                return Promise.reject(error)
+            })
+        },
+        []
+    )
 
     const login = () => {
-        axios.get("api/users", {auth: {username, password: password}})
+        axios.get("api/users/login", {auth: {username, password: password}})
             .then(response => response.data)
-            .then(() => console.log("from login"))
-            .then(hideAndShow)
+            .then(setMe)
+            .catch(() => toast("Username or password is incorrect."))
     }
+
+    const logout = () => {
+        axios.get("api/users/logout")
+            .then(response => response.data)
+            .then(() => setMe("anonymousUser"))
+    }
+
     return <>
-        <div className="login" id="loginContainer">
-            <form onSubmit={login} autoComplete='off' className='form'>
-                <div className='control'>
-                    <h3>
-                        Sign In
-                    </h3>
-                </div>
-                <div className='control block-cube block-input'>
+        {
+            me ? (
+                    me === "anonymousUser" ?
+                        <div className="login" id="loginContainer">
+                            <form onSubmit={login} autoComplete='off' className='form'>
+                                <div className='control'>
+                                    <h3>
+                                        Sign In
+                                    </h3>
+                                </div>
+                                <div className='control block-cube block-input'>
 
-                    <input name='username'
-                           placeholder='Username'
-                           type='text'
-                           onChange={event => setUsername(event.target.value)}/>
+                                    <input name='username'
+                                           placeholder='Username'
+                                           type='text'
+                                           onChange={event => setUsername(event.target.value)}/>
 
-                    <div className='bg-top'>
-                        <div className='bg-inner'></div>
-                    </div>
-                    <div className='bg-right'>
-                        <div className='bg-inner'></div>
-                    </div>
-                    <div className='bg'>
-                        <div className='bg-inner'></div>
-                    </div>
-                </div>
-                <div className='control block-cube block-input'>
+                                    <div className='bg-top'>
+                                        <div className='bg-inner'></div>
+                                    </div>
+                                    <div className='bg-right'>
+                                        <div className='bg-inner'></div>
+                                    </div>
+                                    <div className='bg'>
+                                        <div className='bg-inner'></div>
+                                    </div>
+                                </div>
+                                <div className='control block-cube block-input'>
 
-                    <input name='password'
-                           placeholder='Password'
-                           type='password'
-                           onChange={event => setPassword(event.target.value)}/>
+                                    <input name='password'
+                                           placeholder='Password'
+                                           type='password'
+                                           onChange={event => setPassword(event.target.value)}/>
 
-                    <div className='bg-top'>
-                        <div className='bg-inner'></div>
-                    </div>
-                    <div className='bg-right'>
-                        <div className='bg-inner'></div>
-                    </div>
-                    <div className='bg'>
-                        <div className='bg-inner'></div>
-                    </div>
-                </div>
+                                    <div className='bg-top'>
+                                        <div className='bg-inner'></div>
+                                    </div>
+                                    <div className='bg-right'>
+                                        <div className='bg-inner'></div>
+                                    </div>
+                                    <div className='bg'>
+                                        <div className='bg-inner'></div>
+                                    </div>
+                                </div>
 
-                <button
-                    onClick={login}
-                    className='btn block-cube block-cube-hover'
-                    type='button'>
+                                <button
+                                    onClick={login}
+                                    className='btn block-cube block-cube-hover'
+                                    type='button'>
 
-                    <div className='bg-top'>
-                        <div className='bg-inner'></div>
-                    </div>
-                    <div className='bg-right'>
-                        <div className='bg-inner'></div>
-                    </div>
-                    <div className='bg'>
-                        <div className='bg-inner'></div>
-                    </div>
-                    <div className='text'>
-                        Log In
-                    </div>
-                </button>
+                                    <div className='bg-top'>
+                                        <div className='bg-inner'></div>
+                                    </div>
+                                    <div className='bg-right'>
+                                        <div className='bg-inner'></div>
+                                    </div>
+                                    <div className='bg'>
+                                        <div className='bg-inner'></div>
+                                    </div>
+                                    <div className='text'>
+                                        Log In
+                                    </div>
+                                </button>
 
-            </form>
-        </div>
-
+                            </form>
+                        </div>
+                        : <button onClick={logout}>Logout!</button>
+                )
+                : <div className={"rocket"}></div>
+        }
     </>
 }
+
--- frontend/src/pages/MyAccount.tsx
+++ frontend/src/pages/MyAccount.tsx
@@ -1,8 +1,10 @@
 import Header from "../components/Header";
+import Registration from "../components/Registration";
 
 export default function MyAccount() {
     return <>
         <Header/>
         <p>you are in My Account</p>
+        <Registration/>
     </>
 }
