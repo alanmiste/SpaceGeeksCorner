78e91317ab718334654eda51c212d5386ab86ef2
Alan <baranalaan@gmail.com>
2022-08-24 13:01:59 +0200

    Add AuthenticationIntegrationTest and SgcIntegrationTest.

---
+++ backend/src/test/java/am/alanmiste/spacegeekscorner/sgc/AuthenticationIntegrationTest.java
@@ -0,0 +1,24 @@
+package am.alanmiste.spacegeekscorner.sgc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthenticationIntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void unauthorized() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+                .andExpect(status().is4xxClientError());
+    }
+
+}
