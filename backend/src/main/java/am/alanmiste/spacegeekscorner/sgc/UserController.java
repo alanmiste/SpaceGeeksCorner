abfafb61af477339c52820234ab52e4642ff5f9f
Alan <baranalaan@gmail.com>
2022-09-02 17:09:17 +0200

    Add listusers Controller, POST controller for new users.

--- backend/src/main/java/am/alanmiste/spacegeekscorner/sgc/UserController.java
+++ backend/src/main/java/am/alanmiste/spacegeekscorner/sgc/UserController.java
@@ -1,15 +1,19 @@
 package am.alanmiste.spacegeekscorner.sgc;
 
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
 @RestController
-@RequestMapping(path = "api/users/")
+@RequestMapping(path = "/api/users")
 public class UserController {
+
+    private final AppUserDetailsService appUserDetailsService;
+
+    public UserController(AppUserDetailsService appUserDetailsService) {
+        this.appUserDetailsService = appUserDetailsService;
+    }
 
     @GetMapping("login")
     String login() {
@@ -27,5 +31,16 @@ public class UserController {
     @GetMapping("logout")
     void logout(HttpSession session) {
         session.invalidate();
+    }
+
+    @GetMapping("listusers")
+    Object listUsername() {
+        return appUserDetailsService.listUsers();
+    }
+
+    @PostMapping
+    public String register(@RequestBody NewUser newUser) {
+        appUserDetailsService.register(newUser);
+        return "Done!";
     }
 }
