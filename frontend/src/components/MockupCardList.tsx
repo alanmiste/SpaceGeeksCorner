14058e35157233fb02d55fb1389ef7b85bdf3a9f
Alan <baranalaan@gmail.com>
2022-09-11 16:01:56 +0200

    Add MockupCardList.tsx

---
+++ frontend/src/components/MockupCardList.tsx
@@ -0,0 +1,12 @@
+import MockupCard from "./MockupCard";
+
+export default function MockupCardList() {
+    return <span className="mockupCardListContainer">
+    <MockupCard/>
+    <MockupCard/>
+    <MockupCard/>
+    <MockupCard/>
+    <MockupCard/>
+    <MockupCard/>
+    </span>
+}
