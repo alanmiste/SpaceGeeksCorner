9f4cb156e7c355fa31b514364813b0944a1901a6
Alan <baranalaan@gmail.com>
2022-09-11 14:06:05 +0200

    Update ShowMockup.

--- frontend/src/components/ShowMockup.tsx
+++ frontend/src/components/ShowMockup.tsx
@@ -20,27 +20,28 @@ export default function ShowMockup() {
     };
 
     return <div className="showMockupContainer">
-        <img className="bigImage"
+        <img className="showMockupBigImage"
              src={"https://printful-upload.s3-accelerate.amazonaws.com/tmp/ca7d9467be42df7cef5ef1b77c2b0e02/unisex-staple-t-shirt-black-front-631a233580e16.jpg"}
              alt={"Front Mockup"}/>
-        <div className="mockupDetails">
+        <div className="showMockupDetails">
             <p>The Unisex Staple T-Shirt feels soft and light with just the right amount of stretch. It's comfortable
                 and flattering for all. We can't compliment this shirt enough–it's one of our crowd favorites, and it's
                 sure to be your next favorite too!
             </p>
             <div className="selectThings">
-
-                <label>Select size:</label>
-                <FormControl required sx={{m: 1, minWidth: 100}}>
-                    <InputLabel id="demo-simple-select-required-label">Size</InputLabel>
+                <label className="showMockupLabelText">Select size:</label>
+                <FormControl required>
+                    <InputLabel id="demo-simple-select-required-label"
+                    >Size</InputLabel>
                     <Select
                         labelId="demo-simple-select-required-label"
                         id="demo-simple-select-required"
                         value={size}
                         label="Size *"
                         onChange={handleChange}
+                        sx={{minWidth: 90, height: 50}}
                     >
-                        <MenuItem value="">
+                        <MenuItem value=''>
                             <em>None</em>
                         </MenuItem>
                         <MenuItem value={'XS'}>XS</MenuItem>
@@ -51,8 +52,8 @@ export default function ShowMockup() {
                     </Select>
                 </FormControl>
             </div>
-            <div>
-                <label>Select color: </label>
+            <div className="showMockupSelectColor">
+                <label className="showMockupLabelText">Select color: </label>
                 <input checked={colorChecked} type="radio" onChange={handleColorChange} value="white"
                        className="radioWhiteBtn radioBtn"/>
                 <input checked={!colorChecked} type="radio" onChange={handleColorChange} value="black"
