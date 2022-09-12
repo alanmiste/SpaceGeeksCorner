145f6d449fab8d966da4c6d7a80e4a46983c3d20
Alan <baranalaan@gmail.com>
2022-09-12 21:44:59 +0200

    Add PrintfulResponseTest.

---
+++ backend/src/test/java/am/alanmiste/spacegeekscorner/sgc/model/PrintfulResponseTest.java
@@ -0,0 +1,42 @@
+package am.alanmiste.spacegeekscorner.sgc.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class PrintfulResponseTest {
+
+    PrintfulResponse printfulResponse = new PrintfulResponse(200,
+            new PrintfulResult("gt-404733720", "pending"), new String[]{});
+    PrintfulResponse printfulResponse2 = new PrintfulResponse(200,
+            new PrintfulResult("gt-404733720", "pending"), new String[]{});
+    PrintfulResponse printfulResponse3 = new PrintfulResponse(200,
+            new PrintfulResult("gt-404733720", "done"), new String[]{});
+
+    @Test
+    void testToString() {
+        assertThat(printfulResponse.toString()).hasToString("PrintfulResponse{code=200, result=PrintfulResult[task_key=gt-404733720, status=pending], extra=[]}");
+    }
+
+    @Test
+    void testEquals() {
+        Assertions.assertThat(printfulResponse.equals(printfulResponse2)).isTrue();
+    }
+
+    @Test
+    void testNotEquals() {
+        Assertions.assertThat(printfulResponse.equals(printfulResponse3)).isFalse();
+    }
+
+    @Test
+    void testHashCode() {
+        Assertions.assertThat(printfulResponse.hashCode()).hasSameHashCodeAs(printfulResponse2.hashCode());
+    }
+
+    @Test
+    void testDifferentHashCode() {
+        Assertions.assertThat(printfulResponse.hashCode()).isNotSameAs(printfulResponse3.hashCode());
+    }
+
+}
