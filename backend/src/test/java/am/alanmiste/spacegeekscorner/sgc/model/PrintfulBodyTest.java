dc3a5f0209f3ee09cd8a0802599d7c8d85d2d3ce
Alan <baranalaan@gmail.com>
2022-09-12 22:01:11 +0200

    Fix PrintfulBodyTest.

--- backend/src/test/java/am/alanmiste/spacegeekscorner/sgc/model/PrintfulBodyTest.java
+++ backend/src/test/java/am/alanmiste/spacegeekscorner/sgc/model/PrintfulBodyTest.java
@@ -6,39 +6,37 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class PrintfulBodyTest {
+    int[] variant_ids = {4017, 4018, 4019};
+    PrintfulBodyFiles[] printfulBodyFiles =
+            {new PrintfulBodyFiles("front", "https://apod.nasa.gov/apod/image/1812/ana03BennuVantuyne1024c.jpg",
+                    new PrintfulBodyFilePosition(1800, 2400, 1800, 1800, 300, 0))};
 
-    PrintfulBodyFiles printfulBodyFiles =
-            new PrintfulBodyFiles("front", "https://apod.nasa.gov/apod/image/1812/ana03BennuVantuyne1024c.jpg",
-                    new PrintfulBodyFilePosition(1800, 2400, 1800, 1800, 300, 0));
-    PrintfulBodyFiles printfulBodyFiles2 =
-            new PrintfulBodyFiles("front", "https://apod.nasa.gov/apod/image/1812/ana03BennuVantuyne1024c.jpg",
-                    new PrintfulBodyFilePosition(1800, 2400, 1800, 1800, 300, 0));
-    PrintfulBodyFiles printfulBodyFiles3 =
-            new PrintfulBodyFiles("back", "https://apod.nasa.gov/apod/image/1812/ana03BennuVantuyne1024c.jpg",
-                    new PrintfulBodyFilePosition(1800, 2400, 1800, 1800, 300, 0));
+    PrintfulBody printfulBody = new PrintfulBody(variant_ids, "jpg", printfulBodyFiles);
+    PrintfulBody printfulBody2 = new PrintfulBody(variant_ids, "jpg", printfulBodyFiles);
+    PrintfulBody printfulBody3 = new PrintfulBody(variant_ids, "png", printfulBodyFiles);
 
     @Test
     void testToString() {
-        assertThat(printfulBodyFiles.toString()).hasToString("PrintfulBodyFiles[placement=front, image_url=https://apod.nasa.gov/apod/image/1812/ana03BennuVantuyne1024c.jpg, position=PrintfulBodyFilePosition[area_width=1800, area_height=2400, width=1800, height=1800, top=300, left=0]]");
+        assertThat(printfulBody.toString()).hasToString("PrintfulBody{variant_ids=[4017, 4018, 4019], format='jpg', files=[PrintfulBodyFiles[placement=front, image_url=https://apod.nasa.gov/apod/image/1812/ana03BennuVantuyne1024c.jpg, position=PrintfulBodyFilePosition[area_width=1800, area_height=2400, width=1800, height=1800, top=300, left=0]]]}");
     }
 
     @Test
     void testEquals() {
-        Assertions.assertThat(printfulBodyFiles.equals(printfulBodyFiles2)).isTrue();
+        Assertions.assertThat(printfulBody.equals(printfulBody2)).isTrue();
     }
 
     @Test
     void testNotEquals() {
-        Assertions.assertThat(printfulBodyFiles.equals(printfulBodyFiles3)).isFalse();
+        Assertions.assertThat(printfulBody.equals(printfulBody3)).isFalse();
     }
 
     @Test
     void testHashCode() {
-        Assertions.assertThat(printfulBodyFiles.hashCode()).hasSameHashCodeAs(printfulBodyFiles2.hashCode());
+        Assertions.assertThat(printfulBody.hashCode()).hasSameHashCodeAs(printfulBody2.hashCode());
     }
 
     @Test
     void testDifferentHashCode() {
-        Assertions.assertThat(printfulBodyFiles.hashCode()).isNotSameAs(printfulBodyFiles3.hashCode());
+        Assertions.assertThat(printfulBody.hashCode()).isNotSameAs(printfulBody3.hashCode());
     }
 }
