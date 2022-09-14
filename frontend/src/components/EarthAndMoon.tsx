9839a063fadd21f49d7d7725733b846bc31bf432
Alan <baranalaan@gmail.com>
2022-09-14 19:20:47 +0200

    Fix EarthAndMoon CSS -Position.

--- frontend/src/components/EarthAndMoon.tsx
+++ frontend/src/components/EarthAndMoon.tsx
@@ -1,14 +1,16 @@
 import "./EarthAndMoon.css"
 
 export default function EarthAndMoon() {
-    return <article className="earth-moon">
+    return <div className="earth-moon">
         <div className="earth">
-            <img className="earthImg" src="https://raw.githubusercontent.com/alanmiste/plants/master/earth.png"/>
+            <img className="earthImg" src="https://raw.githubusercontent.com/alanmiste/plants/master/earth.png"
+                 alt="Earth"/>
         </div>
         <div className="moon-container">
             <div className="moon">
-                <img className="moonImg" src="https://raw.githubusercontent.com/alanmiste/plants/master/moon.png"/>
+                <img className="moonImg" src="https://raw.githubusercontent.com/alanmiste/plants/master/moon.png"
+                     alt="Moon"/>
             </div>
         </div>
-    </article>;
+    </div>;
 }
