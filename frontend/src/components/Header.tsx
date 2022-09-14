5212c2b4300bd59f7e5abd002ce88f0a00b81f2b
Alan <baranalaan@gmail.com>
2022-09-14 09:13:15 +0200

    Update Logo.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,10 +5,9 @@ type HeaderProps = {
     me: string,
 }
 export default function Header(props: HeaderProps) {
-
     return (<header>
         <div className="titleAndLogo">
-            <img className="logoImg" src="https://s5.gifyu.com/images/G-2c7d6bdbff81916c3.gif"
+            <img className="logoImg" src="https://raw.githubusercontent.com/alanmiste/plants/master/SGCLogoGIF.gif"
                  alt="Space Geeks Corner Logo"/>
             <h1>Space Geeks Corner</h1>
         </div>
