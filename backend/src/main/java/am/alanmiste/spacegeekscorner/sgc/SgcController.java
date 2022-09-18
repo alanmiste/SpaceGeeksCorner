3beba191c3b558a32d06542423a71cc73fbaa7ba
Alan <baranalaan@gmail.com>
2022-09-18 15:56:16 +0200

    Add listMockup and deleteOneSavedMockup controllers.

--- backend/src/main/java/am/alanmiste/spacegeekscorner/sgc/SgcController.java
+++ backend/src/main/java/am/alanmiste/spacegeekscorner/sgc/SgcController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/sgc")
@@ -59,4 +60,17 @@ public class SgcController {
                 .status(HttpStatus.CREATED)
                 .body(savedTshirt);
     }
+
+    @PostMapping("/list-mockup")
+    public Optional<MockupToSave> listMockup(@RequestBody UsernameType usernameType) {
+        return sgcService.listMockup(usernameType.username());
+    }
+
+    @PutMapping("/list-mockup")
+    public ResponseEntity<Void> deleteOneSavedMockup(
+            @RequestBody DeleteMockup deleteMockup) {
+        boolean deleteSuccess = sgcService.deleteOneSavedMockup(deleteMockup.username(), deleteMockup.index());
+        return new ResponseEntity<>(deleteSuccess ? HttpStatus.NO_CONTENT : HttpStatus.NOT_FOUND);
+    }
+
 }
