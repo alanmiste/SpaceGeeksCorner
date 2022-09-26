c0e11109caa8152c4401bd777af301f5d82f5cdc
alanmiste <baranalaan@gmail.com>
2022-09-26 19:00:16 +0200

    Add DeleteUserDialog component.

---
+++ frontend/src/components/DeleteUserDialog.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function DeleteUserDialog() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Button variant="outlined" color="error" onClick={handleClickOpen}>
+                Delete My Account
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Delete account</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure you want to delete your account?
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Enter your password"
+                        type="email"
+                        fullWidth
+                        variant="standard"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleClose}>Yes, I'm sure!</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
